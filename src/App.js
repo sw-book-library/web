0db9f7b46fb036e59fce0fa85fd50b94d046d788
Douglas Kuerten <100738618+DouglasKuerten@users.noreply.github.com>
2022-06-28 14:10:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -471,17 +471,15 @@ class App extends Component {
     }
     else {
       return (<ul className="quadro-dados">{this.state.emprestimos.map((val, key) => {
-        var nomeUsuario = this.state.username
-        var nomeLivro  = this.state.title
         return (
           <li key={key}>
             <ul className="linha-dados">
               {this.buscarNomeUsuario(val.userId, "LISTAGEM")}
-              {this.buscarNomeLivro(val.bookId, "LISTAGEM")}
+              //{this.buscarNomeLivro(val.bookId, "LISTAGEM")}
               <li style={{ width: '100px' }}>{val.userId}</li>
-              <li style={{ width: '251px' }}>{nomeUsuario}</li>
+              <li style={{ width: '251px' }}>{this.state.username}</li>
               <li style={{ width: '70px' }}>{val.bookId}</li>
-              <li style={{ width: '331px' }}>{nomeLivro}</li>
+              <li style={{ width: '331px' }}>{this.state.title}</li>
               <li style={{ width: '140px', textAlign: 'center' }}>{val.createdAt}</li>
               <li style={{ width: '140px', textAlign: 'center' }}>{ /* new Intl.DateTimeFormat('en-US').format( */val.returnDate/* ) */}</li>
               <li style={{ width: '22px', textAlign: 'center' }}><button onClick={() => { this.deletarEmprestimo(val.id) }}>D</button></li>
@@ -489,6 +487,8 @@ class App extends Component {
             </ul>
           </li>
         );
+            this.setState({ username: "" });
+            this.setState({ title: "" });
       })
       }</ul>);
     }
