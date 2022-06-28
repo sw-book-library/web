953bdec149aa0910b4d438c8e1790fb012943219
Douglas Kuerten <100738618+DouglasKuerten@users.noreply.github.com>
2022-06-28 13:37:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -383,7 +383,7 @@ class App extends Component {
         if(local === "INPUT"){
           document.getElementById("nome-usuario").value = res.data.name 
         }else if (local === "LISTAGEM"){
-          return {res.data.name}
+          return res.data.name
         }
       }
       });
@@ -399,7 +399,7 @@ class App extends Component {
           if(local === "INPUT"){
             document.getElementById("nome-livro").value = res.data.title
           }else if (local === "LISTAGEM"){
-            return {res.data.title}
+            return res.data.title
           }
         }
       });
